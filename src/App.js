a00d98f0b95c2d3ba5d680d48bcef5c5618f9880
sedx876 <swatkins876@gmail.com>
2020-10-18 05:56:42 -0400

    Added enter.js component and enter.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Enter from './components/Enter'
+
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Enter />
     </div>
   );
 }
